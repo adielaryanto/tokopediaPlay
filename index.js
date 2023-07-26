5db67db2937385bd20e0ae0cdb592912a8c43c4e
adielaryanto <adiel.aryanto@gmail.com>
2023-07-26 14:20:27 +0700

    update index.js

---
+++ index.js
@@ -0,0 +1,64 @@
+require('dotenv').config();
+
+const bodyParser = require('body-parser');
+const express = require ('express');
+const mongoose = require('mongoose');
+const mongoString = process.env.DATABASE_URL;
+const http = require('http');
+
+
+mongoose.connect(mongoString);
+
+const database = mongoose.connection;
+
+database.on('error',(error)=>{
+    console.log(error);
+})
+
+database.once('connected', ()=>{
+    console.log('Database Connected');
+})
+
+const routes = require('./routes/routes')
+const app = express();
+const server = http.createServer(app);
+
+app.use(bodyParser.json());
+app.use(
+    bodyParser.urlencoded({
+        extended:true,
+    }),
+);
+
+// Skema tiap attribut
+const videoThumbnailSchema = mongoose.Schema({
+    videoId : String,
+    urlImage :String,
+    thumbnail :String
+
+})
+const productListSchema = mongoose.Schema({
+    productId : String,
+    linkProduct : String,
+    title :String,
+    price : Number
+
+})
+const commentListSchema = mongoose.Schema({
+    username : String,
+    comment : String,
+    timestamp :Date
+
+})
+
+// Buat connection API
+app.use('/api',routes)
+
+app.listen(3000,()=>{
+    console.log('Servernya jalan di ${3000}')
+})
+
+// panggil setiap model
+const VideoThumbnailList = mongoose.model('VideoThumbnailList', videoThumbnailSchema);
+
+
